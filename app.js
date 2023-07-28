f429d81e84cc275bca2058fd9ab94c6ee41002a9
AndreaDC <andrea.dclercq@gmail.com>
2023-07-28 20:57:47 +0200

    export events

--- app.js
+++ app.js
@@ -26,36 +26,18 @@ for (const folder of commandFolders){
     }
 }
 
-client.on(Events.InteractionCreate, async interaction => {
-    if(!interaction.isChatInputCommand()) return;
-    
-    const command = interaction.client.commands.get(interaction.commandName);
-    if(!command) {
-        console.error(`No commande matching ${interaction.commandName} was found.`);
-        return;
-    }
+const eventsPath = path.join(__dirname, 'events');
+const eventsFile = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
 
-    try {
-        await command.execute(interaction);
-    } catch (error){
-        console.log(error);
-        if( interaction.replied || interaction.deferred) {
-            await interaction.followUp({
-                content: 'There was an error while executing this command!', ephemeral: true
-            })
-        } else {
-            await interaction.reply({
-                content: 'There was an error while executing this command!', ephemeral: true
-            })
-        }
+for(const file of eventsFile){
+    const filePath = path.join(eventsPath, file)
+    const event = require(filePath)
+    if(event.once){
+        client.once(event.name, (...args) => event.execute(...args));
+    } else {
+        client.on(event.name, (...args) => event.execute(...args));
     }
-})
-
-// When the client is ready, run this code (only once)
-// We use 'c' for the event parameter to keep it separate from the already defined 'client'
-client.once(Events.ClientReady, c => {
-	console.log(`Ready! Logged in as ${c.user.tag}`);
-});
+}
 
 // Log in to Discord with your client's token
 client.login(process.env.DISCORD_TOKEN);
