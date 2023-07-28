adedcd3f4f817b72b9d0d55646d80b6a8ab95e3d
AndreaDC <andrea.dclercq@gmail.com>
2023-07-28 19:23:19 +0200

    init gitignore for config

---
+++ app.js
@@ -0,0 +1,23 @@
+const Discord = require("discord.js")
+const client = new Discord.Client()
+
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`)
+})
+
+client.on("message", msg => {
+  if (msg.content === "ping") {
+    msg.reply("pong");
+  }
+})
+
+client.login(process.env.TOKEN)
+
+// import "dotenv/config";
+// import express from 'express'
+
+// const app = express();
+
+// const PORT = process.env.PORT || 3000;
+
+// app.use.(express.json({ verify: Verfi}))
